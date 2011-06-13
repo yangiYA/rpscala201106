940b9c4f7efbe0ccafa8c55f79f5899086a85362
hyt1900 <pc@ti.que.jp>
2011-06-14 02:56:47 +0900

    trait Matrix2D 廃止するなどリファクタリング

--- src/main/scala/jp/que/ti/ygame/m/core/Matrix.scala
+++ src/main/scala/jp/que/ti/ygame/m/core/Matrix.scala
@@ -5,7 +5,7 @@ package jp.que.ti.ygame.m.core {
  * インスタンス生成は、object [[jp.que.ti.ygame.m.core.Matrix]]の apply メソッドを使用する。
  * @author yhj
  */
-sealed abstract class Matrix {
+sealed trait Matrix {
   /**[[scala.Int]]型のx座標 */
   def xByInt: Int
 
@@ -68,21 +68,32 @@ sealed abstract class Matrix {
    * @param scalarValue 倍数
    * @return 行列の実数倍
    */
-  def product(scalarValue: Long): Matrix = Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  def product(scalarValue: Long): Matrix = scalarValue match {
+    case -1 => NegativeMatrixWithDouble(this)
+    case other => Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  }
+
 
   /**
    * この行列の実数倍(引数)を返却する
    * @param scalarValue 倍数
    * @return 行列の実数倍
    */
-  def product(scalarValue: Int): Matrix = Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  def product(scalarValue: Int): Matrix = scalarValue match {
+    case -1 => NegativeMatrixWithDouble(this)
+    case other => Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  }
+
 
   /**
    * この行列の実数倍(引数)を返却する
    * @param scalarValue 倍数
    * @return 行列の実数倍
    */
-  def product(scalarValue: Double): Matrix = Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  def product(scalarValue: Double): Matrix = scalarValue match {
+    case -1 => NegativeMatrixWithDouble(this)
+    case other => Matrix(x * scalarValue, y * scalarValue, z * scalarValue)
+  }
 
   /**
    * この行列に実数倍(引数)した行列と、この行列との和を返却する
@@ -110,22 +121,40 @@ sealed abstract class Matrix {
  * [[jp.que.ti.ygame.m.core.Matrix]]のインスタンスを生成するためのシングルトンオブジェクト
  */
 object Matrix {
-  /**[[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド */
-  def apply(x: Int, y: Int, z: Int): MatrixWithInt = new MatrixWithInt(x, y, z)
+  /**
+   * [[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド
+   * @param x X座標
+   * @param y Y座標
+   * @param z Z座標
+   */
+  def apply(x: Int, y: Int, z: Int): MatrixWithInt = MatrixWithInt(x, y, z)
 
-  /**[[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド */
-  def apply(x: Int, y: Int): MatrixWithInt2D = new MatrixWithInt2D(x, y)
+  /**
+   * [[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド
+   * @param x X座標
+   * @param y Y座標
+   */
+  def apply(x: Int, y: Int): MatrixWithInt2D = MatrixWithInt2D(x, y)
 
-  /**[[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド */
-  def apply(x: Double, y: Double, z: Double): MatrixWithDouble = new MatrixWithDouble(x, y, z)
+  /**
+   * [[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド
+   * @param x X座標
+   * @param y Y座標
+   * @param z Z座標
+   */
+  def apply(x: Double, y: Double, z: Double): MatrixWithDouble = MatrixWithDouble(x, y, z)
 
-  /**[[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド */
-  def apply(x: Double, y: Double): MatrixWithDouble2D = new MatrixWithDouble2D(x, y)
+  /**
+   * [[jp.que.ti.ygame.m.core.Matrix]]インスタンス生成メソッド
+   * @param x X座標
+   * @param y Y座標
+   */
+  def apply(x: Double, y: Double): MatrixWithDouble2D = MatrixWithDouble2D(x, y)
 
   /**すべての要素がIntの最小値である[[jp.que.ti.ygame.m.core.Matrix]]インスタンス */
-  val intMin: Matrix = new MatrixWithInt(Int.MinValue, Int.MinValue, Int.MinValue)
+  val intMin: Matrix = MatrixWithInt(Int.MinValue, Int.MinValue, Int.MinValue)
   /**すべての要素が0である[[jp.que.ti.ygame.m.core.Matrix]]インスタンス */
-  val Zero: Matrix = new MatrixWithInt2D(0, 0)
+  val Zero: Matrix = MatrixWithInt2D(0, 0)
 }
 
 /**
@@ -134,12 +163,8 @@ object Matrix {
  * このクラスのコンストラクタはprivateです。インスタンス生成は、
  * object [[jp.que.ti.ygame.m.core.Matrix]]の apply メソッドを使用してください。
  * @author yhj
- *
- * @param xpos X座標
- * @param ypos Y座標
- * @param zpos Z座標
  */
-class MatrixWithDouble private[core](val x: Double, val y: Double, val z: Double) extends Matrix {
+trait MatrixWithDouble extends Matrix {
   def xByInt: Int = x.intValue
 
   def yByInt: Int = y.intValue
@@ -147,18 +172,20 @@ class MatrixWithDouble private[core](val x: Double, val y: Double, val z: Double
   def zByInt: Int = z.intValue
 }
 
+private[core] object MatrixWithDouble {
+  def apply(x: Double, y: Double, z: Double): MatrixWithDouble = new MatrixWithDoubleImpl(x, y, z)
+}
+
+private[core] class MatrixWithDoubleImpl private[core](val x: Double, val y: Double, val z: Double) extends MatrixWithDouble
+
 /**
  * 座標を表すクラス。当クラスは imutable です
  * <br/>
  * このクラスのコンストラクタはprivateです。インスタンス生成は、
  * object [[jp.que.ti.ygame.m.core.Matrix]]の apply メソッドを使用してください。
  * @author yhj
- *
- * @param xpos X座標
- * @param ypos Y座標
- * @param zpos Z座標
  */
-class MatrixWithInt private[core](val xByInt: Int, val yByInt: Int, val zByInt: Int) extends Matrix {
+trait MatrixWithInt extends Matrix {
 
   override def x = xByInt
 
@@ -174,33 +201,21 @@ class MatrixWithInt private[core](val xByInt: Int, val yByInt: Int, val zByInt: 
   override def add(matrix: MatrixWithInt2D): MatrixWithInt =
     Matrix(xByInt + matrix.xByInt, yByInt + matrix.yByInt, zByInt)
 
-  override def product(scalarValue: Int): MatrixWithInt =
-    Matrix(xByInt * scalarValue, yByInt * scalarValue, zByInt * scalarValue)
+  override def product(scalarValue: Int): MatrixWithInt = scalarValue match {
+    case -1 => NegativeMatrixWithInt(this)
+    case other => Matrix(xByInt * scalarValue, yByInt * scalarValue, zByInt * scalarValue)
+  }
+
 
   override def productValueAndAdd(scalarValue: Int): Matrix =
     Matrix(xByInt + xByInt * scalarValue, yByInt + yByInt * scalarValue, zByInt + zByInt * scalarValue)
 }
 
-/**
- * Z座標は0固定の座標を表すtrait
- * @author yhj
- */
-private[core] trait Matrix2D extends Matrix {
-  def zByInt: Int = 0
-
-  override def toString: String = getClass.getSimpleName + "(" + x + " ," + y + ")"
-
-  override def product(scalarValue: Double): MatrixWithDouble2D = new MatrixWithDouble2D(x * scalarValue, y * scalarValue)
-
-  override def product(scalarValue: Long): MatrixWithDouble2D = new MatrixWithDouble2D(x * scalarValue, y * scalarValue)
-
-  override def productValueAndAdd(scalarValue: Double): MatrixWithDouble2D =
-    new MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
-
-  override def productValueAndAdd(scalarValue: Long): MatrixWithDouble2D =
-    new MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
-
+private[core] object MatrixWithInt {
+  def apply(x: Int, y: Int, z: Int): MatrixWithInt = new MatrixWithIntImpl(x, y, z)
 }
+
+private[core] class MatrixWithIntImpl private[core](val xByInt: Int, val yByInt: Int, val zByInt: Int) extends MatrixWithInt
 
 /**
  * 座標を表すクラス。Z座標は0固定のクラスです。当クラスは imutable です
@@ -208,28 +223,55 @@ private[core] trait Matrix2D extends Matrix {
  * このクラスのコンストラクタはprivateです。インスタンス生成は、
  * object [[jp.que.ti.ygame.m.core.Matrix]]の apply メソッドを使用してください。
  * @author yhj
- *
- * @param xpos X座標
- * @param ypos Y座標
  */
-class MatrixWithDouble2D private[core](val x: Double, val y: Double) extends Matrix2D {
+trait MatrixWithDouble2D extends Matrix {
+  override def toString: String = getClass.getSimpleName + "(" + x + " ," + y + ")"
+
   def xByInt: Int = x.intValue
 
   def yByInt: Int = y.intValue
+
+  def zByInt: Int = 0
 
   def z: Double = zByInt
 
   override def add(matrix: Matrix): Matrix = matrix.add(this)
 
-  override def add(matrix: MatrixWithDouble2D): MatrixWithDouble2D = new MatrixWithDouble2D(x + matrix.x, y + matrix.y)
+  override def add(matrix: MatrixWithDouble2D): MatrixWithDouble2D = MatrixWithDouble2D(x + matrix.x, y + matrix.y)
 
-  override def add(matrix: MatrixWithInt2D): MatrixWithDouble2D = new MatrixWithDouble2D(x + matrix.x, y + matrix.y)
+  override def add(matrix: MatrixWithInt2D): MatrixWithDouble2D = MatrixWithDouble2D(x + matrix.x, y + matrix.y)
 
-  override def product(scalarValue: Int): MatrixWithDouble2D = new MatrixWithDouble2D(x * scalarValue, y * scalarValue)
+
+  override def product(scalarValue: Int): MatrixWithDouble2D = scalarValue match {
+    case -1 => NegativeMatrixWithDouble2D(this)
+    case other => Matrix(x * scalarValue, y * scalarValue)
+  }
+
+  override def product(scalarValue: Double): MatrixWithDouble2D = scalarValue match {
+    case -1 => NegativeMatrixWithDouble2D(this)
+    case other => MatrixWithDouble2D(x * scalarValue, y * scalarValue)
+  }
+
+  override def product(scalarValue: Long): MatrixWithDouble2D = scalarValue match {
+    case -1 => NegativeMatrixWithDouble2D(this)
+    case other => MatrixWithDouble2D(x * scalarValue, y * scalarValue)
+  }
+
+  override def productValueAndAdd(scalarValue: Double): MatrixWithDouble2D =
+    MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
+
+  override def productValueAndAdd(scalarValue: Long): MatrixWithDouble2D =
+    MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
 
   override def productValueAndAdd(scalarValue: Int): MatrixWithDouble2D =
-    new MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
+    MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
 }
+
+private[core] object MatrixWithDouble2D {
+  def apply(x: Double, y: Double): MatrixWithDouble2D = new MatrixWithDouble2DImpl(x, y)
+}
+
+private[core] class MatrixWithDouble2DImpl private[core](val x: Double, val y: Double) extends MatrixWithDouble2D
 
 /**
  * 座標を表すクラス。Z座標は0固定のクラスです。当クラスは imutable です
@@ -237,11 +279,12 @@ class MatrixWithDouble2D private[core](val x: Double, val y: Double) extends Mat
  * このクラスのコンストラクタはprivateです。インスタンス生成は、
  * object [[jp.que.ti.ygame.m.core.Matrix]]の apply メソッドを使用してください。
  * @author yhj
- *
- * @param xpos X座標
- * @param ypos Y座標
  */
-class MatrixWithInt2D private[core](val xByInt: Int, val yByInt: Int) extends Matrix2D {
+trait MatrixWithInt2D extends Matrix {
+  override def toString: String = getClass.getSimpleName + "(" + x + " ," + y + ")"
+
+  def zByInt: Int = 0
+
   override def x = xByInt
 
   override def y = yByInt
@@ -251,52 +294,126 @@ class MatrixWithInt2D private[core](val xByInt: Int, val yByInt: Int) extends Ma
   override def add(matrix: Matrix): Matrix = matrix.add(this)
 
   override def add(matrix: MatrixWithInt2D): MatrixWithInt2D =
-    new MatrixWithInt2D(xByInt + matrix.xByInt, yByInt + matrix.yByInt)
+    MatrixWithInt2D(xByInt + matrix.xByInt, yByInt + matrix.yByInt)
 
   override def add(matrix: MatrixWithInt): MatrixWithInt =
-    new MatrixWithInt(xByInt + matrix.xByInt, yByInt + matrix.yByInt, matrix.zByInt)
+    MatrixWithInt(xByInt + matrix.xByInt, yByInt + matrix.yByInt, matrix.zByInt)
 
   override def add(matrix: MatrixWithDouble2D): MatrixWithDouble2D =
-    new MatrixWithDouble2D(x + matrix.x, y + matrix.y)
+    MatrixWithDouble2D(x + matrix.x, y + matrix.y)
 
-  override def product(scalarValue: Int): MatrixWithInt2D =
-    new MatrixWithInt2D(xByInt * scalarValue, yByInt * scalarValue)
+  override def product(scalarValue: Int): MatrixWithInt2D = scalarValue match {
+    case -1 => NegativeMatrixWithInt2D(this)
+    case other => MatrixWithInt2D(xByInt * scalarValue, yByInt * scalarValue)
+  }
+
+  override def product(scalarValue: Double): MatrixWithDouble2D = MatrixWithDouble2D(x * scalarValue, y * scalarValue)
+
+  override def product(scalarValue: Long): MatrixWithDouble2D = MatrixWithDouble2D(x * scalarValue, y * scalarValue)
+
+  override def productValueAndAdd(scalarValue: Double): MatrixWithDouble2D =
+    MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
+
+  override def productValueAndAdd(scalarValue: Long): MatrixWithDouble2D =
+    MatrixWithDouble2D(x + x * scalarValue, y + y * scalarValue)
 
   override def productValueAndAdd(scalarValue: Int): MatrixWithInt2D =
-    new MatrixWithInt2D(xByInt + xByInt * scalarValue, yByInt + yByInt * scalarValue)
+    MatrixWithInt2D(xByInt + xByInt * scalarValue, yByInt + yByInt * scalarValue)
 }
 
-abstract trait NegativeMatrix extends Matrix {
+private[core] object MatrixWithInt2D {
+  def apply(x: Int, y: Int): MatrixWithInt2D = new MatrixWithInt2DImpl(x, y)
+}
+
+private[core] class MatrixWithInt2DImpl private[core](val xByInt: Int, val yByInt: Int) extends MatrixWithInt2D
+
+trait NegativeMatrix extends Matrix {
   type t <: Matrix
   val matrix: t
 
   /**[[scala.Int]]型のx座標 */
-  def xByInt: Int = matrix.xByInt * -1
+  override def xByInt: Int = matrix.xByInt * -1
 
   /**[[scala.Int]]型のy座標 */
-  def yByInt: Int = matrix.yByInt * -1
+  override def yByInt: Int = matrix.yByInt * -1
 
   /**[[scala.Int]]型のz座標 */
-  def zByInt: Int = matrix.zByInt * -1
+  override def zByInt: Int = matrix.zByInt * -1
 
   /**x座標 */
-  def x: Double = matrix.x * -1
+  override def x: Double = matrix.x * -1
 
   /**y座標 */
-  def y: Double = matrix.y * -1
+  override def y: Double = matrix.y * -1
 
   /**z座標 */
-  def z: Double = matrix.z * -1
+  override def z: Double = matrix.z * -1
+}
+
+final private[core] case class
+NegativeMatrixWithDouble(override val matrix: Matrix
+                          ) extends MatrixWithDouble with NegativeMatrix {
+  type t = Matrix
+
+  override def product(scalarValue: Long): Matrix = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+
+  override def product(scalarValue: Int): Matrix = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+
+  override def product(scalarValue: Double): Matrix = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
 
 }
 
 final private[core] case class
-NegativeMatrixWithInt2D(
-                         override val matrix: MatrixWithInt2D
-                         ) extends NegativeMatrix with Matrix2D {
+NegativeMatrixWithInt(override val matrix: MatrixWithInt
+                       ) extends MatrixWithInt with NegativeMatrix {
+  type t = MatrixWithInt
+
+  override def product(scalarValue: Int): MatrixWithInt = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+
+}
+
+final private[core] case class
+NegativeMatrixWithDouble2D(override val matrix: MatrixWithDouble2D
+                            ) extends MatrixWithDouble2D with NegativeMatrix {
+  type t = MatrixWithDouble2D
+
+  override def product(scalarValue: Int): MatrixWithDouble2D = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+
+  override def product(scalarValue: Long): MatrixWithDouble2D = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+
+  override def product(scalarValue: Double): MatrixWithDouble2D = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
+}
+
+final private[core] case class
+NegativeMatrixWithInt2D(override val matrix: MatrixWithInt2D
+                         ) extends MatrixWithInt2D with NegativeMatrix {
   type t = MatrixWithInt2D
 
-  override def zByInt = 0
+  override def product(scalarValue: Int): MatrixWithInt2D = scalarValue match {
+    case -1 => matrix
+    case other => super.product(scalarValue)
+  }
 }
 
 }
