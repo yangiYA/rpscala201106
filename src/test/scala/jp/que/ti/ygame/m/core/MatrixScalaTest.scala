373bbf3f86a40670e959a3108d0da2ae73ba90d1
hyt1900 <pc@ti.que.jp>
2011-06-14 02:57:33 +0900

    trait Matrix2D廃止などリファクタリング

--- src/test/scala/jp/que/ti/ygame/m/core/MatrixScalaTest.scala
+++ src/test/scala/jp/que/ti/ygame/m/core/MatrixScalaTest.scala
@@ -13,22 +13,22 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
 
   @Test def createIntObjectTest() {
     val actualInt = Matrix(1, 2, 3)
-    assert(actualInt === new MatrixWithInt(1, 2, 3))
+    assert(actualInt === MatrixWithInt(1, 2, 3))
     assert(actualInt.isInstanceOf[MatrixWithInt], actualInt + ",の型をテスト")
 
     val actual2d = Matrix(1, 2)
-    assert(actual2d === new MatrixWithInt2D(1, 2))
+    assert(actual2d === MatrixWithInt2D(1, 2))
     assert(actual2d.isInstanceOf[MatrixWithInt2D], actual2d + ",の型をテスト")
   }
 
   @Test def addIntObjectTest() {
-    val mtx21_32_0 = new MatrixWithInt2D(21, 32)
+    val mtx21_32_0 = MatrixWithInt2D(21, 32)
     assert((Matrix(1, 2) + Matrix(20, 30)) === mtx21_32_0)
     assert((Matrix(20, 30) + Matrix(1, 2)) === mtx21_32_0)
     assert((Matrix(1, 2).add(Matrix(20, 30))) === mtx21_32_0)
     assert((Matrix(1, 2) + Matrix(20, 30)).isInstanceOf[MatrixWithInt2D] === true)
 
-    val mtx21_32_50 = new MatrixWithInt(21, 32, 50)
+    val mtx21_32_50 = MatrixWithInt(21, 32, 50)
     assert((Matrix(1, 2) + Matrix(20, 30, 50)) === mtx21_32_50)
     assert(Matrix(1, 2).add(Matrix(20, 30, 50)) === mtx21_32_50)
     assert(Matrix(20, 30, 50) + Matrix(1, 2) === mtx21_32_50)
@@ -36,29 +36,29 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def productIntObjectTest {
-    val mtx40_60_0 = new MatrixWithInt2D(40, 60)
+    val mtx40_60_0 = MatrixWithInt2D(40, 60)
     val actualInt2d = Matrix(20, 30).product(2)
     assert(actualInt2d === mtx40_60_0)
     assert(actualInt2d.isInstanceOf[MatrixWithInt2D], actualInt2d + ",の型をテスト")
 
-    val mtx40_60_90 = new MatrixWithInt(40, 60, 90)
+    val mtx40_60_90 = MatrixWithInt(40, 60, 90)
     val actualInt = Matrix(20, 30, 45).product(2)
     assert(actualInt === mtx40_60_90)
     assert(actualInt.isInstanceOf[MatrixWithInt], actualInt + ",の型をテスト")
   }
 
   @Test def productValueAndAddIntObjectTest {
-    val mtx120_180_0 = new MatrixWithInt2D(120, 180)
+    val mtx120_180_0 = MatrixWithInt2D(120, 180)
     val actualInt2d = Matrix(20, 30).productValueAndAdd(5)
     assert(Matrix(20, 30).productValueAndAdd(5) === mtx120_180_0)
     assert(actualInt2d.isInstanceOf[MatrixWithInt2D], actualInt2d + ",の型をテスト")
 
-    val mtx120_180_240 = new MatrixWithInt(120, 180, 240)
+    val mtx120_180_240 = MatrixWithInt(120, 180, 240)
     val actualInt = Matrix(20, 30, 40).productValueAndAdd(5)
     assert(actualInt === mtx120_180_240)
     assert(actualInt.isInstanceOf[MatrixWithInt], actualInt + ",の型をテスト")
 
-    val mtx120_180_240Double = new MatrixWithDouble(120, 180, 240)
+    val mtx120_180_240Double = MatrixWithDouble(120, 180, 240)
     assert(Matrix(20, 30, 40).productValueAndAdd(5.0) === mtx120_180_240Double)
     assert(Matrix(20, 30, 40).productValueAndAdd(5L) === mtx120_180_240Double)
     assert(Matrix(20, 30, 40).productValueAndAdd(5f) === mtx120_180_240Double)
@@ -68,7 +68,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def createDoubleObjectTest() {
-    val mtx10_20_30Double = new MatrixWithDouble(1.0, 2.0, 3.0)
+    val mtx10_20_30Double = MatrixWithDouble(1.0, 2.0, 3.0)
     assert(Matrix(1.0, 2.0, 3.0) === mtx10_20_30Double)
     assert(Matrix(1.0, 2, 3) === mtx10_20_30Double)
     assert(Matrix(1, 2.0, 3) === mtx10_20_30Double)
@@ -76,11 +76,11 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
 
     assert(Matrix(1.1, 2.2, 3.3) != Matrix(1, 2, 3)) //少数点以下が切り捨てられた等号判定になっていないかのテスト
 
-    assert(Matrix(1.1, 2.2) === new MatrixWithDouble2D(1.1, 2.2))
+    assert(Matrix(1.1, 2.2) === MatrixWithDouble2D(1.1, 2.2))
     assert(Matrix(1.1, 2.2) != Matrix(1, 2)) //少数点以下が切り捨てられた等号判定になっていないかのテスト
 
 
-    val mtx10_20_0Double = new MatrixWithDouble2D(1.0, 2.0)
+    val mtx10_20_0Double = MatrixWithDouble2D(1.0, 2.0)
     assert(Matrix(1.0, 2.0) === mtx10_20_0Double)
     assert(Matrix(1, 2.0) === mtx10_20_0Double)
     assert(Matrix(1.0, 2) === mtx10_20_0Double)
@@ -92,7 +92,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def addDouble2DObjectTest() {
-    val mtx111_222_0 = new MatrixWithDouble2D(11.1, 22.2)
+    val mtx111_222_0 = MatrixWithDouble2D(11.1, 22.2)
     assert(Matrix(1.0, 2.2) + Matrix(10.1, 20) === mtx111_222_0)
     assert(Matrix(1.0, 2.2).add(Matrix(10.1, 20)) === mtx111_222_0)
     assert(Matrix(10.1, 20) + Matrix(1.0, 2.2) === mtx111_222_0)
@@ -110,7 +110,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def addDouble3DObjectTest() {
-    val mtx111_222_333 = new MatrixWithDouble(11.1, 22.2, 33.3)
+    val mtx111_222_333 = MatrixWithDouble(11.1, 22.2, 33.3)
     assert(Matrix(1.0, 2.2, 3.3) + Matrix(10.1, 20.0, 30.0) === mtx111_222_333)
     assert(Matrix(10.1, 20.0, 30.0) + Matrix(1.0, 2.2, 3.3) === mtx111_222_333)
     assert(Matrix(1.0, 2.2, 3.3).add(Matrix(10.1, 20.0, 30.0)) === mtx111_222_333)
@@ -133,7 +133,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def productDouble2DObjectTest {
-    val mtx400_600_0 = new MatrixWithDouble2D(40.0, 60.0)
+    val mtx400_600_0 = MatrixWithDouble2D(40.0, 60.0)
     assert(Matrix(20.0, 30.0).product(2) === mtx400_600_0)
     assert(Matrix(20.0, 30.0).product(2.0) === mtx400_600_0)
     assert(Matrix(20.0, 30.0).product(2L) === mtx400_600_0)
@@ -147,7 +147,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def productDouble3DObjectTest {
-    val mtx400_600_900 = new MatrixWithDouble(40.2, 60.2, 90.2)
+    val mtx400_600_900 = MatrixWithDouble(40.2, 60.2, 90.2)
     assert(Matrix(20.1, 30.1, 45.1).product(2) === mtx400_600_900)
     assert(Matrix(20.1, 30.1, 45.1).product(2.0) === mtx400_600_900)
     assert(Matrix(20.1, 30.1, 45.1).product(2L) === mtx400_600_900)
@@ -155,7 +155,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def productValueAndAddDouble2DObjectTest {
-    val mtx120_180_0 = new MatrixWithDouble2D(120.6, 180.6)
+    val mtx120_180_0 = MatrixWithDouble2D(120.6, 180.6)
     assert(Matrix(20.1, 30.1).productValueAndAdd(5) === mtx120_180_0)
     assert(Matrix(20.1, 30.1).productValueAndAdd(5.0) === mtx120_180_0)
     assert(Matrix(20.1, 30.1).productValueAndAdd(5L) === mtx120_180_0)
@@ -163,7 +163,7 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   }
 
   @Test def productValueAndAddDouble3DObjectTest {
-    val mtx120_180_240 = new MatrixWithDouble(120.6, 180.6, 240.6)
+    val mtx120_180_240 = MatrixWithDouble(120.6, 180.6, 240.6)
     assert(Matrix(20.1, 30.1, 40.1).productValueAndAdd(5) === mtx120_180_240)
     assert(Matrix(20.1, 30.1, 40.1).productValueAndAdd(5.0) === mtx120_180_240)
     assert(Matrix(20.1, 30.1, 40.1).productValueAndAdd(5L) === mtx120_180_240)
@@ -173,60 +173,60 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
   @Test def addTest {
     var m1: Matrix = Matrix(1.1, 2.2, 3.3)
     var m2: Matrix = Matrix(10.01, 20.02, 30.03)
-    assert(m1 + m2 === new MatrixWithDouble(11.11, 22.22, 33.33))
+    assert(m1 + m2 === MatrixWithDouble(11.11, 22.22, 33.33))
 
     m2 = Matrix(10, 20, 30)
-    assert(m1 + m2 === new MatrixWithDouble(11.1, 22.2, 33.3))
-    assert(m2 + m1 === new MatrixWithDouble(11.1, 22.2, 33.3))
+    assert(m1 + m2 === MatrixWithDouble(11.1, 22.2, 33.3))
+    assert(m2 + m1 === MatrixWithDouble(11.1, 22.2, 33.3))
 
     m2 = Matrix(10, 20)
-    assert(m1 + m2 === new MatrixWithDouble(11.1, 22.2, 3.3))
-    assert(m2 + m1 === new MatrixWithDouble(11.1, 22.2, 3.3))
+    assert(m1 + m2 === MatrixWithDouble(11.1, 22.2, 3.3))
+    assert(m2 + m1 === MatrixWithDouble(11.1, 22.2, 3.3))
 
     m2 = Matrix(10.01, 20.02)
-    assert(m1 + m2 === new MatrixWithDouble(11.11, 22.22, 3.3))
-    assert(m2 + m1 === new MatrixWithDouble(11.11, 22.22, 3.3))
+    assert(m1 + m2 === MatrixWithDouble(11.11, 22.22, 3.3))
+    assert(m2 + m1 === MatrixWithDouble(11.11, 22.22, 3.3))
 
     //
 
     m1 = Matrix(1, 2, 3)
     m2 = Matrix(10, 20, 30)
     val actualInt = m1 + m2
-    assert(actualInt === new MatrixWithInt(11, 22, 33))
+    assert(actualInt === MatrixWithInt(11, 22, 33))
     assert(actualInt.isInstanceOf[MatrixWithInt], actualInt + ",の型をテスト")
 
 
     m2 = Matrix(10.1, 20.2, 30.3)
-    assert(m1 + m2 === new MatrixWithDouble(11.1, 22.2, 33.3))
-    assert(m2 + m1 === new MatrixWithDouble(11.1, 22.2, 33.3))
+    assert(m1 + m2 === MatrixWithDouble(11.1, 22.2, 33.3))
+    assert(m2 + m1 === MatrixWithDouble(11.1, 22.2, 33.3))
 
     m2 = Matrix(10, 20)
-    assert(m1 + m2 === new MatrixWithInt(11, 22, 3))
-    assert(m2 + m1 === new MatrixWithInt(11, 22, 3))
+    assert(m1 + m2 === MatrixWithInt(11, 22, 3))
+    assert(m2 + m1 === MatrixWithInt(11, 22, 3))
     assert((m1 + m2).isInstanceOf[MatrixWithInt] === true)
 
     m2 = Matrix(10.1, 20.2)
-    assert(m1 + m2 === new MatrixWithDouble(11.1, 22.2, 3))
-    assert(m2 + m1 === new MatrixWithDouble(11.1, 22.2, 3))
+    assert(m1 + m2 === MatrixWithDouble(11.1, 22.2, 3))
+    assert(m2 + m1 === MatrixWithDouble(11.1, 22.2, 3))
 
     //
 
     m1 = Matrix(1, 2)
     m2 = Matrix(10, 20)
     val actualInt2d = m1 + m2
-    assert(actualInt2d === new MatrixWithInt2D(11, 22))
+    assert(actualInt2d === MatrixWithInt2D(11, 22))
     assert(actualInt2d.isInstanceOf[MatrixWithInt2D], actualInt2d + ",の型をテスト")
 
     m2 = Matrix(10.1, 20.2)
-    assert(m1 + m2 === new MatrixWithDouble2D(11.1, 22.2))
-    assert(m2 + m1 === new MatrixWithDouble2D(11.1, 22.2))
+    assert(m1 + m2 === MatrixWithDouble2D(11.1, 22.2))
+    assert(m2 + m1 === MatrixWithDouble2D(11.1, 22.2))
     assert((m2 + m1).isInstanceOf[MatrixWithDouble2D] === true)
 
     //
     m1 = Matrix(1.1, 2.2)
     m2 = Matrix(10.01, 20.02, 0.3)
-    assert(m1 + m2 === new MatrixWithDouble(11.11, 22.22, 0.3))
-    assert(m2 + m1 === new MatrixWithDouble(11.11, 22.22, 0.3))
+    assert(m1 + m2 === MatrixWithDouble(11.11, 22.22, 0.3))
+    assert(m2 + m1 === MatrixWithDouble(11.11, 22.22, 0.3))
   }
 
   @Test def hashCodeTest {
@@ -249,7 +249,91 @@ class MatrixScalaTest extends JUnitSuite with ShouldMatchersForJUnit {
     assert(map(Matrix(2.0, 1L, 0.10)) === 30)
     assert(map.contains(Matrix(1, 2, 0)) === false)
   }
-  @Test def NegativeMatrixTest {
+
+  @Test def negativeMatrixTest {
+    //Int2D
+    val mInt2D = Matrix(1, 2)
+    assert(mInt2D.product(-1) === Matrix(-1, -2))
+    assert(mInt2D.product(-1L) === Matrix(-1, -2))
+    assert(mInt2D.product(-1.0) === Matrix(-1, -2))
+    assert(mInt2D.product(-1.0f) === Matrix(-1, -2))
+
+    assert((mInt2D.product(-1)).isInstanceOf[NegativeMatrixWithInt2D])
+
+    //2DIntがDoubleに精度が変わる掛け算はNegativeMatrix対応しないことにする
+    //assert((mInt2D.product(-1L)).isInstanceOf[NegativeMatrixWithDouble2D])
+    //assert((mInt2D.product(-1.0)).isInstanceOf[NegativeMatrixWithDouble2D])
+    //assert((mInt2D.product(-1.0f)).isInstanceOf[NegativeMatrixWithDouble2D])
+
+    assert((mInt2D.product(-1).product(-1)) === mInt2D)
+    assert((mInt2D.product(-1L).product(-1L)) === mInt2D)
+    assert((mInt2D.product(-1.0).product(-1.0)) === mInt2D)
+    assert((mInt2D.product(-1).product(-1)) eq mInt2D) //オブジェクトが使いまわされることをテスト
+
+    assert((mInt2D.product(-1).product(-1.0).product(-1L).product(-1f)) === mInt2D)
+
+    //
+    //Int3D
+    val mInt3D = Matrix(1, 2, 3)
+    assert(mInt3D.product(-1) === Matrix(-1, -2, -3))
+    assert(mInt3D.product(-1L) === Matrix(-1, -2, -3))
+    assert(mInt3D.product(-1.0) === Matrix(-1, -2, -3))
+    assert(mInt3D.product(-1.0f) === Matrix(-1, -2, -3))
+
+    assert((mInt3D.product(-1)).isInstanceOf[NegativeMatrixWithInt])
+
+    assert((mInt3D.product(-1L)).isInstanceOf[NegativeMatrixWithDouble])
+    assert((mInt3D.product(-1.0)).isInstanceOf[NegativeMatrixWithDouble])
+    assert((mInt3D.product(-1.0f)).isInstanceOf[NegativeMatrixWithDouble])
+
+    assert((mInt3D.product(-1).product(-1)) === mInt3D)
+    assert((mInt3D.product(-1L).product(-1L)) === mInt3D)
+    assert((mInt3D.product(-1.0).product(-1.0)) === mInt3D)
+    assert((mInt3D.product(-1).product(-1)) eq mInt3D) //2回-1を掛けるとオブジェクトが使いまわされることをテスト
+
+    assert((mInt3D.product(-1).product(-1.0).product(-1L).product(-1f)) === mInt3D)
+
+
+    //Double2D
+    val mDouble2D = Matrix(1.1, 2.2)
+    assert(mDouble2D.product(-1) === Matrix(-1.1, -2.2))
+    assert(mDouble2D.product(-1L) === Matrix(-1.1, -2.2))
+    assert(mDouble2D.product(-1.0) === Matrix(-1.1, -2.2))
+    assert(mDouble2D.product(-1.0f) === Matrix(-1.1, -2.2))
+
+    assert((mDouble2D.product(-1)).isInstanceOf[NegativeMatrixWithDouble2D])
+
+    assert((mDouble2D.product(-1L)).isInstanceOf[NegativeMatrixWithDouble2D])
+    assert((mDouble2D.product(-1.0)).isInstanceOf[NegativeMatrixWithDouble2D])
+    assert((mDouble2D.product(-1.0f)).isInstanceOf[NegativeMatrixWithDouble2D])
+
+    assert((mDouble2D.product(-1).product(-1)) === mDouble2D)
+    assert((mDouble2D.product(-1L).product(-1L)) === mDouble2D)
+    assert((mDouble2D.product(-1.0).product(-1.0)) === mDouble2D)
+    assert((mDouble2D.product(-1).product(-1)) eq mDouble2D) //2回-1を掛けるとオブジェクトが使いまわされることをテスト
+
+    assert((mDouble2D.product(-1).product(-1.0).product(-1L).product(-1f)) === mDouble2D)
+
+
+    //Double3D
+    val mDouble3D = Matrix(1.1, 2.2, 3.3)
+    assert(mDouble3D.product(-1) === Matrix(-1.1, -2.2, -3.3))
+    assert(mDouble3D.product(-1L) === Matrix(-1.1, -2.2, -3.3))
+    assert(mDouble3D.product(-1.0) === Matrix(-1.1, -2.2, -3.3))
+    assert(mDouble3D.product(-1.0f) === Matrix(-1.1, -2.2, -3.3))
+
+    assert((mDouble3D.product(-1)).isInstanceOf[NegativeMatrixWithDouble])
+    assert((mDouble3D.product(-1L)).isInstanceOf[NegativeMatrixWithDouble])
+    assert((mDouble3D.product(-1.0)).isInstanceOf[NegativeMatrixWithDouble])
+    assert((mDouble3D.product(-1.0f)).isInstanceOf[NegativeMatrixWithDouble])
+
+    assert((mDouble3D.product(-1).product(-1)) === mDouble3D)
+    assert((mDouble3D.product(-1L).product(-1L)) === mDouble3D)
+    assert((mDouble3D.product(-1.0).product(-1.0)) === mDouble3D)
+    assert((mDouble3D.product(-1).product(-1)) eq mDouble3D) //2回-1を掛けるとオブジェクトが使いまわされることをテスト
+
+    assert((mDouble3D.product(-1).product(-1.0).product(-1L).product(-1f)) === mDouble3D)
+
   }
 }
 
